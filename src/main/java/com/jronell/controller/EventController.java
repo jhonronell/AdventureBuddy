223f818f90412fd3ab250ef7e1a909205c8f8e3a
jhonronell <jhonronell@gmail.com>
2015-08-22 17:44:11 +0800

    First Commit

---
+++ src/main/java/com/jronell/controller/EventController.java
@@ -0,0 +1,51 @@
+package com.jronell.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class EventController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EventController.class);
+	
+	
+	
+	@RequestMapping(value="addEvent", method=RequestMethod.GET)
+	public String addEvent(Locale locale,Model model){
+		logger.info("Welcome home! Addevebt {}.", locale);
+	
+		
+		
+		
+		
+		return null;
+	}
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index";
+	}
+	
+}
